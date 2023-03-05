712bdcc1bfd4833533969163418c2adcc8ce7e45
Kesava moorthy <kesav143@yahoo.com>
2023-03-05 13:20:14 +0530

    Day1-Version1

---
+++ 1.py
@@ -0,0 +1,11 @@
+print ("Hello world" * 10)
+c=10+5
+print (c)
+if 5>15:
+    print ("5 is greater") #Greater logic
+    print ("Hai")
+else:
+    print ("15 is greater")
+print(type(c))
+y_x=10
+print (y_x)
